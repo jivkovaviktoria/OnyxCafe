2cf910eca1f585ef54f63f29fa1ff57c325e45de
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-04 21:20:18 +0300

    Added function to scroll to the top when the page is refreshed

--- JS/homepage.js
+++ JS/homepage.js
@@ -2,3 +2,10 @@ $(document).on('click', 'ul li', function (){
     $(this).addClass('active').siblings().removeClass('active')
 })
 
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
